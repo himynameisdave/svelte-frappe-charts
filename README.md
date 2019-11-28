3de7458a5adbde33b6ce3a34ea87fe652c52eb00
himynameisdave <dave@fossa.com>
2019-11-28 10:50:08 -0800

    ✌️ Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# svelte-frappe-charts
-📈 Svelte bindings for frappe-charts
+<div align="center" margin="0 auto 20px">
+  <h1>svelte-frappe-charts</h1>
+  <p style="font-style: italic;">📈 Svelte bindings for <a href="https://frappe.io/charts">frappe-charts.</a></p>
+  <div>
+    
+  </div>
+</div>
+
+--- 
+
+
+Makes it easy to use [frappe-charts](https://frappe.io/charts) in your [Svelte](https://svelte.dev/) project.
+
+### Installation
+
+```
+yarn add svelte svelte-frappe-charts
+
+npm i -S svelte svelte-frappe-charts
+```
+
+> **Note**: Assumes you are using `>= svelte@3.0.0`.
+
+### Usage
+
+...coming soon...
+
+---
+
+_✌️Created by [Dave](http://himynameisdave.com). Licenced under MIT._
---
+++ src/chart.svelte
@@ -0,0 +1,45 @@
+<script>
+  // import { onMount, afterUpdate, onDestroy } from 'svelte';
+  import { onMount } from 'svelte';
+  import { Chart } from "frappe-charts/dist/frappe-charts.esm";
+
+  /**
+   *  Expected frappe-charts data
+   */ 
+  export let data = {
+    labels: [],
+    datasets: [
+      {
+        values: [],
+      }
+    ],
+  };
+
+  let chart = null;
+
+  onMount(() => {
+    console.log('hello world!');
+    chart = new Chart("#chart", {
+      data: data,
+      type: 'line',
+      height: 300,
+      axisOptions: {
+        xIsSeries: true,
+        xAxisMode: 'tick',
+      },
+      lineOptions: {
+        dotSize: 7,
+        spline: true,
+      },
+      tooltipOptions: {
+        formatTooltipY: d => d + 'km',
+      },
+      valuesOverPoints: 1,
+      colors: ['#6a9ae5']
+    });
+  });
+
+</script>
+
+
+<div id="chart"></div>
